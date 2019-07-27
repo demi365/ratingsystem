b966f394172bbc9861cda3c34fb40f987b7b997b
demi365 <deepak1212365@gmail.com>
2019-07-27 23:00:12 +0530

    Replacing sqlite in favour of H2:file DB Added lombok dependency

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USER_DETAILS (first_name, last_name, email) VALUES
+  ('Crazy', 'Persists', 'abcd@gmail.com'),
+  ('Coole', 'Cozy', 'testing@email.com'),
+  ('Comfy', 'Indiana', 'wiz@marvel.com');
