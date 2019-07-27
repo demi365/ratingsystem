6783d5e153af64ff17e80ad87eaf15b781b5fcbc
demi365 <deepak1212365@gmail.com>
2019-07-27 18:25:48 +0530

    Creating the folder structure Adding files at proper places Added sqlite dependency

---
+++ src/main/java/learning/spring/boot/controller/Starter.java
@@ -0,0 +1,21 @@
+package learning.spring.boot.controller;
+
+import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Starter {
+
+    /**
+     * Trying out mapping using variables
+     */
+    
+    private static final String HOME = "home";
+
+    @GetMapping("/")
+    public String viewIndex() {
+        return HOME;
+    }
+
+}
